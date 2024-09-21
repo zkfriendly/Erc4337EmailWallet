678db8cbdc3a827ca4e94ba3063435fd504ae007
zkfriendly <zkfriendly@proton.me>
2024-09-21 14:50:24 +0200

    fix: DKIM registary access storage to decide validity

--- contracts/contracts/MockDkimRegistry.sol
+++ contracts/contracts/MockDkimRegistry.sol
@@ -2,11 +2,18 @@
 
 pragma solidity ^0.8.23;
 
+// accesses global storage to determine if a DKIM public key hash is valid
 contract HMockDkimRegistry {
+    uint256 public isInvalidStorage;
+
+    constructor() {
+        isInvalidStorage = 5;
+    }
+
     function isDKIMPublicKeyHashValid(
         uint256 publicKeyHash
-    ) public pure returns (bool) {
-        if (publicKeyHash == 5) {
+    ) public view returns (bool) {
+        if (publicKeyHash == isInvalidStorage) {
             return false;
         }
         return true;
