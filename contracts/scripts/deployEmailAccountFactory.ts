d9a9db11525f0c8248c8f2b74a2e2e567d7babb0
zkfriendly <zkfriendly@proton.me>
2024-09-23 12:31:56 +0200

    feat: factory deploy script

---
+++ contracts/scripts/deployEmailAccountFactory.ts
@@ -0,0 +1,39 @@
+import { ethers } from "hardhat";
+(async() => {
+    const bundlerProvider = new ethers.JsonRpcProvider(
+      "http://localhost:3000/rpc"
+    );
+
+    // get list of supported entrypoints
+    const entrypoints = await bundlerProvider.send(
+      "eth_supportedEntryPoints",
+      []
+    );
+
+    if (entrypoints.length === 0) {
+      throw new Error("No entrypoints found");
+    }
+
+    const entryPointAddress = entrypoints[0];
+
+    // deploy the verifier
+    const verifier = await ethers.getContractFactory("EmailAccountDummyVerifier");
+    const verifierContract = await verifier.deploy();
+    await verifierContract.waitForDeployment();
+    const verifierAddress = await verifierContract.getAddress();
+
+    // deploy the dkim registry
+    const dkimRegistry = await ethers.getContractFactory("HMockDkimRegistry");
+    const dkimRegistryContract = await dkimRegistry.deploy();
+    await dkimRegistryContract.waitForDeployment();
+    const dkimRegistryAddress = await dkimRegistryContract.getAddress();
+
+    // deploy the factory
+    const factory = await ethers.getContractFactory("EmailAccountFactory");
+    const factoryContract = await factory.deploy(entryPointAddress, verifierAddress, dkimRegistryAddress);
+    await factoryContract.waitForDeployment();
+    const factoryAddress = await factoryContract.getAddress();
+    
+    console.log("Factory deployed at", factoryAddress);
+
+})();
