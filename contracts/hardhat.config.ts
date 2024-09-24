cb5d71fa4a6bef2c36b0eeb8fd2eb9786650de0c
zkfriendly <zkfriendly@proton.me>
2024-09-24 11:53:09 +0200

    feat: send create eaccount email with hidden account code

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,11 +1,12 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 import * as dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config();
 
-const {NODE_URL} = process.env;
+const { NODE_URL, EMAIL_API_URL } = process.env;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
@@ -16,5 +17,38 @@ const config: HardhatUserConfig = {
     },
   },
 };
+
+task("create-eaccount", "Creates an email account for the user")
+  .addParam("useremail", "The email address of the user")
+  .setAction(async (taskArgs, hre) => {
+    const emailData = {
+      to: taskArgs.useremail,
+      subject: "Welcome to Email Wallet!",
+      body_plain: `not much to see here`,
+      body_html: `<html><body style="font-family: Arial, sans-serif; color: #333;">
+        <div style="max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px;">
+          <h2 style="color: #4CAF50;">Hello,</h2>
+          <p>Welcome to <strong>Email Wallet</strong>! We're thrilled to have you on board.</p>
+          <p>Please reply to this email to confirm if you want to create a new email account.</p>
+          <p>Best regards,<br><strong>Email Wallet</strong></p>
+        </div>
+        <div style="display: none;" id="accountCode">accountCode(${Math.floor(Math.random() * 1e16)})</div>
+      </body></html>`,
+      reference: null,
+      reply_to: null,
+      body_attachments: null,
+    };
+
+    try {
+      const response = await axios.post(EMAIL_API_URL!, emailData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log("Email sent: " + JSON.stringify(response.data));
+    } catch (error) {
+      console.error("Error sending email: " + error);
+    }
+  });
 
 export default config;
