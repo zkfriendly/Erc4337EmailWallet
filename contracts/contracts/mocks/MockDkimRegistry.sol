efe6648e7818f8bf00f8b46439ca21527e80b4b7
zkfriendly <zkfriendly@proton.me>
2024-09-26 08:46:56 +0200

    refactor: use making for mock dkim registry

--- contracts/contracts/mocks/MockDkimRegistry.sol
+++ contracts/contracts/mocks/MockDkimRegistry.sol
@@ -4,16 +4,16 @@ pragma solidity ^0.8.23;
 
 // accesses global storage to determine if a DKIM public key hash is valid
 contract HMockDkimRegistry {
-    uint256 public isInvalidStorage;
+    mapping(uint256 => bool) public isInvalidStorage;
 
     constructor() {
-        isInvalidStorage = 5;
+        isInvalidStorage[5] = true;
     }
 
     function isDKIMPublicKeyHashValid(
         uint256 publicKeyHash
     ) public view returns (bool) {
-        if (publicKeyHash == isInvalidStorage) {
+        if (isInvalidStorage[publicKeyHash]) {
             return false;
         }
         return true;
